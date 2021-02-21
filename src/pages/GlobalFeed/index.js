fcc22aa392dd3655f5570c01a98e34268ac9bcce
= <arturakk2015@gmail.com>
2021-02-21 17:15:32 +0300

    added pagination logic

--- src/pages/GlobalFeed/index.js
+++ src/pages/GlobalFeed/index.js
@@ -1,16 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Feed from './../../components/Feed';
 import useFetch from './../../hooks/useFetch';
+import Pagination from './../../components/Pagination';
+import { getPaginator, limit } from './../../utils'; 
+import { stringify } from 'query-string';
 
-const GlobalFeed = () => {
-  const apiUrl = '/articles?limit=10&offset=0';
+const GlobalFeed = ({ location, match }) => {
+  const {offset, currentPage} = getPaginator(location.search)
+  const stringifiedParams = stringify({
+    limit,
+    offset
+  })
+  const apiUrl = `/articles?${stringifiedParams}`;
   const [{response, isLoading, error}, doFetch] = useFetch(apiUrl)
-
-  console.log('response', response)
+  const url = match.url;
 
   useEffect(() => {
     doFetch()
-  }, [doFetch])
+  }, [doFetch, currentPage])
 
   return (
     <div className='home-page'>
@@ -26,7 +33,10 @@ const GlobalFeed = () => {
             {isLoading && <div>Loading...</div>}
             {error && <div>Some error happened</div>}
             {!isLoading && response && (
-              <Feed articles={response.articles} />
+              <Fragment>
+                <Feed articles={response.articles} />
+                <Pagination total={response.articlesCount} limit={limit} url={url} currentPage={currentPage} />
+              </Fragment>
             )}
           </div>
           <div className='col-md-3'>

d0a0a31e84454a9f7a2ab171c58e49ca46f05ecf
= <arturakk2015@gmail.com>
2021-02-22 12:24:26 +0300

    added TagFeed component

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import GlobalFeed from './pages/GlobalFeed';
 import Article from './pages/Article';
 import Auth from './pages/Auth';
+import TagFeed from './pages/TagFeed';
 
 const Router = () => {
   return (
     <Switch>
       <Route path='/' component={GlobalFeed} exact />
+      <Route path='/tags/:slug' component={TagFeed} />
       <Route path='/articles' component={Article} />
       <Route path='/login' component={Auth} />
       <Route parh='/register' component={Auth} />
